b407ff8fc5a011e5266336659e5d7c131284ce26
Josef Utbult <josef.utbult@hotmail.com>
2018-02-15 14:54:50 +0100

    Fixed merge conflict

--- src/lab4/GomokuMain.java
+++ src/lab4/GomokuMain.java
@@ -1,6 +1,5 @@
 package lab4;
 
-<<<<<<< HEAD
 import lab4.client.*;
 
 public class GomokuMain {
@@ -10,20 +9,5 @@ public class GomokuMain {
 		System.out.println("Begin");
 		
 		GomokuClient client = new GomokuClient(1);
-=======
-<<<<<<< HEAD
-public class GomokuMain {
-	
-	public static void GomokuMain(String[] args) {
-=======
-public class Main {
-
-	public static void main(String[] args) {
-
-		System.out.println("Hello world!");
->>>>>>> 86055f50bc204e527086c894391c68792ea446a3
->>>>>>> 7ae6971a832bac2ae663770df06a966eaa69ce84
-		
 	}
-
 }
