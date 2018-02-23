8d308982db6930134305912184546871a06fddf3
Josef Utbult <josef.utbult@hotmail.com>
2018-02-23 13:34:11 +0100

    .

--- src/lab4/GomokuMain.java
+++ src/lab4/GomokuMain.java
@@ -11,16 +11,17 @@ public class GomokuMain {
 	public static void main(String[] args) {
 
 		System.out.println("Begin");
-		GomokuClient client = new GomokuClient(8000);
+		GomokuClient client = new GomokuClient(8005);
 		
 		/*
 		 * Humanize is a flag that changes the appearance of the player-bricks
 		 */
 		GomokuGameState gameState = new GomokuGameState(client, args.length > 0 && args[0].equals("-humanize"));
 		
-		gameState.getGameGrid().move(1, 3, 1);
+		/*gameState.getGameGrid().move(1, 3, 1);
 		gameState.getGameGrid().move(3, 3, 2);
 		gameState.getGameGrid().move(10, 5, 1);
+		*/
 		
 		
 		GomokuGUI gui = new GomokuGUI(gameState, client);
